4a7d56e436381079ef60ab23c744d059877a515c
Rohan Iyer <rohan.iyer1999@gmail.com>
2015-07-14 08:42:29 -0700

    dank

--- datahandler.js
+++ datahandler.js
@@ -2,10 +2,14 @@ var Firebase = require("firebase")
 
 var ref = new Firebase('https://dankripplz.firebaseio.com')
 var count = 0
-ref.on('child_added', function(snapshot){
+ref.child('totaldanks').once('value', function(snapshot){
+	count = snapshot.val()
+	ref.on('child_added', function(snapshot){
 	if(snapshot.key() != 'totaldanks' && snapshot.key() != 'presence'){
 		ref.child(snapshot.key()).remove()
 		count++
 	}
 	ref.child('totaldanks').set(count)
 })
+})
+
